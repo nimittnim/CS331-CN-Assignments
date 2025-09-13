58f70f240846393ffab2d2f9541dd556c8099068
Nimitt <nimittnim@gmail.com>
2025-09-13 17:40:39 +0530

    task1 and task2

--- A1/Task1/client.py
+++ A1/Task1/client.py
@@ -17,7 +17,7 @@ d. ID: Sequence of DNS query starting from 00
 '''
 
 # Importing Libraries
-from scapy.all import rdpcap, DNS
+from scapy.all import PcapReader, DNS, UDP
 import socket, time
 
 # Globals
@@ -28,15 +28,20 @@ LOCAL = False
 OUTPUT_FILE = "client_ans.txt"
 
 def make_header(seq):
-    # add time and seq number
+    ''' return current time + seq number in bits '''
     ts = time.strftime("%H%M%S", time.localtime())
     return f"{ts}{seq:02d}".encode()
 
 def main():
-    pkts = rdpcap(PCAP_FILE)
+    ''' send DNS packets with header to server '''
+
+    # reading pcap file
+    pkts = PcapReader(PCAP_FILE)
+
+    # creating a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # open file once, overwrite on each run
+    # writing response received
     with open(OUTPUT_FILE, "w") as out:
         out.write("Custom Header Value | Domain Name | Resolved IP Address\n")
         out.write("-------------------------------------------------------\n")
@@ -44,21 +49,28 @@ def main():
         seq = 0
 
         for p in pkts:
-            if p.haslayer(DNS) and p[DNS].qr == 0:  # DNS query
-                qname = p[DNS].qd.qname.decode()
-                if qname.endswith(".local.") and not LOCAL:   # skip mDNS Queries
-                    continue
+            # Getting only DNS query and skipping mDNS 
+            if p.haslayer(DNS) and p[DNS].qr == 0 and p[UDP].dport == 53 and not LOCAL:  
+                qname = p[DNS].qd.qname.decode()                                        
+                
+                # if qname.endswith(".local.") and not LOCAL:   # other logic for skipping mDNS Queries
+                #     continue
 
+                # making header
                 header = make_header(seq)
                 payload = header + bytes(p[DNS])
                 sock.sendto(payload, (SERVER_IP, SERVER_PORT))
 
+                # receiving response from server
                 try:
                     data, _ = sock.recvfrom(2048)
-                    resolved = data.decode()
+                    # resolved = data.decode()
+                    resolved = DNS(data).an[0].rdata
+                    
                 except:
                     resolved = "No Reply"
-
+                
+                # logging to file
                 line = f"{header.decode()} | {qname} | {resolved}"
                 out.write(line + "\n")
 
--- A1/Task1/server.py
+++ A1/Task1/server.py
@@ -23,17 +23,19 @@ Rules:
 --------------------------------------------------------------------------------
 '''
 
+# importing libraries
 import socket
-from scapy.layers.dns import DNS
+from scapy.layers.dns import DNS, DNSRR
 
+# Globals
 PORT = 23
 OUTPUT_FILE = "server_response.txt"
 
 # IP pool
 IP_POOL = [
-    "192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5",
-    "192.168.1.6", "192.168.1.7", "192.168.1.8", "192.168.1.9", "192.168.1.10",
-    "192.168.1.11", "192.168.1.12", "192.168.1.13", "192.168.1.14", "192.168.1.15"
+    "192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5", # Morning 
+    "192.168.1.6", "192.168.1.7", "192.168.1.8", "192.168.1.9", "192.168.1.10", # Afternoon
+    "192.168.1.11", "192.168.1.12", "192.168.1.13", "192.168.1.14", "192.168.1.15" # Night
 ]
 
 def pick_ip(header):
@@ -58,41 +60,61 @@ def pick_ip(header):
     return IP_POOL[index]
 
 def main():
+    ''' Recieve DNS Query and send DNS response with resolved IP address '''
+
+    #Intializing UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", PORT))
+
     print(f"Server listening on port {PORT}...")
     print("Server Response")
     print("---------------------------------------------------------------------------------")
     print("Custom Header Value | Domain Name | Resolved IP Address")
     print("---------------------------------------------------------------------------------")
 
-    # open file once, overwrite at start
+    # log the server response
     with open(OUTPUT_FILE, "w") as out:
         out.write("Custom Header Value | Domain Name | Resolved IP Address\n")
         out.write("-------------------------------------------------------\n")
 
     while True:
+
+        # recieve DNS query from client
         data, addr = sock.recvfrom(4096)
+
+        #extract header
         header = data[:8].decode(errors="ignore")
         dns_bytes = data[8:]
+        dns_packet = DNS(dns_bytes)
 
+        # Extract domain name
         qname = "unknown"
         try:
             qname = DNS(dns_bytes).qd.qname.decode()
         except:
             pass
 
+        # resolve ip
         ip = pick_ip(header)
-        reply = ip.encode()
-        sock.sendto(reply, addr)
+
+        # creating DNS packet for response
+        reply = DNS(
+                    id=dns_packet.id,
+                    qr=1,
+                    aa=1,
+                    qd=dns_packet.qd,
+                    an=DNSRR(rrname=qname, ttl=300, rdata=ip)
+                )
+        
+        # sending DNS packet to client
+        sock.sendto(bytes(reply), addr)
 
         line = f"{header} | {qname} | {ip}"
         print(line)
         
-
+        # logging
         with open(OUTPUT_FILE, "a") as out:
             out.write(line + "\n")
-
 
 if __name__ == "__main__":
     main()
